1ff9855102a0000151e9b1cd82655e046295eba5
GabrielEdid <gabrieledid28@gmail.com>
2023-11-15 12:12:51 -0600

    Mas cambios y adiciones a la pantalla inicial

--- src/screens/InitialScreen.js
+++ src/screens/InitialScreen.js
@@ -1,30 +1,60 @@
 import React from "react";
-import { View, Text, StyleSheet, Image, ImageBackground } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
 
 const InitialScreen = ({}) => {
   return (
     //Fondo de pantalla
     <ImageBackground
-      source={require("../../Images/Fondo.png")}
+      source={require("../../assets/images/Fondo.png")}
       style={{ flex: 1 }}
     >
-      //Logo de Tankef
-      <View style={{ flex: 1 }}>
-        <Image
-          source={require("../../Images/Logo_Tankef.png")}
-          style={styles.imagen}
-        />
+      {/*Logo de Tankef*/}
+      <Image
+        source={require("../../assets/images/Logo_Tankef.png")}
+        style={styles.imagen}
+      />
+      {/*Contenedor de los botones*/}
+      <View style={styles.container}>
+        <TouchableOpacity style={{ marginBottom: 10 }}>
+          <Text
+            style={{
+              color: "#29364d",
+              textAlign: "center",
+              fontSize: 16,
+              fontWeight: "bold",
+            }}
+          >
+            Ya tengo una cuenta
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.boton}>
+          <Text
+            style={{
+              color: "white",
+              textAlign: "center",
+              fontSize: 22,
+              fontFamily: "Conthrax",
+            }}
+          >
+            CREAR CUENTA
+          </Text>
+        </TouchableOpacity>
       </View>
-      //Contenedor de los botones
-      <View style={styles.container}></View>
     </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 500,
-    height: 250,
+    marginTop: 455,
+    height: 240,
     justifyContent: "flex-end",
     backgroundColor: "white",
   },
@@ -33,6 +63,15 @@ const styles = StyleSheet.create({
     height: 100,
     alignSelf: "center",
     marginTop: 60,
+  },
+  boton: {
+    width: 350,
+    height: 60,
+    alignSelf: "center",
+    justifyContent: "center",
+    marginBottom: 40,
+    backgroundColor: "#29364d",
+    borderRadius: 25,
   },
 });
 
