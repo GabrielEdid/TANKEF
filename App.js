725a703aed94441207e25b0d5ab3bcb69a541a7c
GabrielEdid <gabrieledid28@gmail.com>
2023-11-19 21:25:07 -0600

    Cambios y mejoras

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import AppLoading from "expo-app-loading";
+import * as Font from "expo-font";
 import InitialScreen from "./src/screens/InitialScreen";
 
 // Create a stack navigator
@@ -20,6 +22,23 @@ function LoginFlow() {
 
 // Main App Component
 export default function App() {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  async function loadFonts() {
+    await Font.loadAsync({
+      conthrax: require("./assets/fonts/conthrax.ttf"),
+    });
+    setFontsLoaded(true);
+  }
+
+  useEffect(() => {
+    loadFonts();
+  }, []);
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <NavigationContainer>
       <LoginFlow />
