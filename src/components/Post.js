8bf97735dc35e2073b5f13f17bf05ebb76a511af
GabrielEdid <gabrieledid28@gmail.com>
2024-01-05 16:03:04 -0600

    Se mejora el scroll view de la pantalla de inicio y se crea el archivo del componente post

---
+++ src/components/Post.js
@@ -0,0 +1,40 @@
+// Importaciones de React Native y React
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+
+const Post = (props) => {
+  return (
+    <View style={styles.Cuadro}>
+      <Text style={styles.textoTitulo}>{props.titulo}</Text>
+      <Text style={styles.textoBody}>{props.body}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  Cuadro: {
+    borderWidth: 1.5,
+    borderRadius: 13,
+    borderColor: "#29364d",
+    height: 98,
+    width: 157,
+    marginEnd: 10,
+    left: 20,
+  },
+  textoTitulo: {
+    fontSize: 15,
+    color: "#29364d",
+    fontWeight: "bold",
+    textAlign: "center",
+    paddingTop: 15,
+  },
+  textoBody: {
+    fontSize: 20,
+    color: "#29364d",
+    fontWeight: "bold",
+    textAlign: "center",
+    paddingTop: 15,
+  },
+});
+
+export default Post;
--- src/screens/Main/Inicio.js
+++ src/screens/Main/Inicio.js
@@ -10,83 +10,97 @@ import React from "react";
 import { LinearGradient } from "expo-linear-gradient";
 // Importaciones de Hooks y Componentes
 import CuadroRedUsuario from "../../components/CuadroRedUsuario";
+import Post from "../../components/Post";
 
 const Inicio = () => {
   // Componente visual
   return (
-    <View style={styles.background}>
-      {/* Titulo */}
-      <Text style={styles.titulo}>TANKEF</Text>
-      {/* Lista de Datos de Red del Usuario */}
-      <ScrollView
-        horizontal={true}
-        showsHorizontalScrollIndicator={false}
-        style={styles.scroll}
-      >
-        <CuadroRedUsuario titulo="Valor de Red" body="$253,500.00" />
-        <CuadroRedUsuario titulo="Mi Crédito" body="$15,000.00" />
-        <CuadroRedUsuario titulo="Mi Inversión" body="$15,000.00" />
-        <CuadroRedUsuario titulo="Obligado Solidario" body="$7,500.00" />
+    <>
+      <View style={styles.tituloContainer}>
+        {/* Titulo */}
+        <Text style={styles.titulo}>TANKEF</Text>
+      </View>
+      <ScrollView style={styles.scrollV}>
+        {/* Lista de Datos de Red del Usuario */}
+        <ScrollView
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          style={styles.scrollH}
+        >
+          <CuadroRedUsuario titulo="Valor de Red" body="$253,500.00" />
+          <CuadroRedUsuario titulo="Mi Crédito" body="$15,000.00" />
+          <CuadroRedUsuario titulo="Mi Inversión" body="$15,000.00" />
+          <CuadroRedUsuario titulo="Obligado Solidario" body="$7,500.00" />
+        </ScrollView>
+        {/* Anuncio para Invertir */}
+        <TouchableOpacity style={styles.cuadroInvertir}>
+          <Text style={styles.texto}>
+            Transforma tu ahorro en inversión, con una{" "}
+            <Text style={{ fontWeight: "bold" }}>
+              Tasa Anual del 13.51% desde $5,000
+            </Text>
+          </Text>
+          <LinearGradient
+            colors={["#2FF690", "#21B6D5"]}
+            start={{ x: 1, y: 1 }} // Inicio del gradiente
+            end={{ x: 0, y: 0 }} // Fin del gradiente
+            style={styles.botonGradient}
+          >
+            <Text
+              style={{
+                fontFamily: "conthrax",
+                color: "white",
+                textAlign: "center",
+                fontSize: 17,
+              }}
+            >
+              INVERTIR
+            </Text>
+          </LinearGradient>
+        </TouchableOpacity>
+        {/* Anuncio para hacer un Crédito */}
+        <TouchableOpacity style={styles.cuadroCredito}>
+          <Text style={[styles.texto, { color: "#29364d" }]}>
+            ¿Necesitas un <Text style={{ fontWeight: "bold" }}>impuslo</Text>{" "}
+            para alcanzar tus <Text style={{ fontWeight: "bold" }}>sueños</Text>
+            ? ¡Da el primer paso hacía tus objetivos!
+          </Text>
+          <LinearGradient
+            colors={["#2FF690", "#21B6D5"]}
+            start={{ x: 1, y: 1 }} // Inicio del gradiente
+            end={{ x: 0, y: 0 }} // Fin del gradiente
+            style={styles.botonGradient}
+          >
+            <Text
+              style={{
+                fontFamily: "conthrax",
+                color: "white",
+                textAlign: "center",
+                fontSize: 17,
+              }}
+            >
+              CRÉDITO
+            </Text>
+          </LinearGradient>
+        </TouchableOpacity>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
       </ScrollView>
-      {/* Anuncio para Invertir */}
-      <TouchableOpacity style={styles.cuadroInvertir}>
-        <Text style={styles.texto}>
-          Transforma tu ahorro en inversión, con una{" "}
-          <Text style={{ fontWeight: "bold" }}>
-            Tasa Anual del 13.51% desde $5,000
-          </Text>
-        </Text>
-        <LinearGradient
-          colors={["#2FF690", "#21B6D5"]}
-          start={{ x: 1, y: 1 }} // Inicio del gradiente
-          end={{ x: 0, y: 0 }} // Fin del gradiente
-          style={styles.botonGradient}
-        >
-          <Text
-            style={{
-              fontFamily: "conthrax",
-              color: "white",
-              textAlign: "center",
-              fontSize: 17,
-            }}
-          >
-            INVERTIR
-          </Text>
-        </LinearGradient>
-      </TouchableOpacity>
-      {/* Anuncio para hacer un Crédito */}
-      <TouchableOpacity style={styles.cuadroCredito}>
-        <Text style={[styles.texto, { color: "#29364d" }]}>
-          ¿Necesitas un <Text style={{ fontWeight: "bold" }}>impuslo</Text> para
-          alcanzar tus <Text style={{ fontWeight: "bold" }}>sueños</Text>? ¡Da
-          el primer paso hacía tus objetivos!
-        </Text>
-        <LinearGradient
-          colors={["#2FF690", "#21B6D5"]}
-          start={{ x: 1, y: 1 }} // Inicio del gradiente
-          end={{ x: 0, y: 0 }} // Fin del gradiente
-          style={styles.botonGradient}
-        >
-          <Text
-            style={{
-              fontFamily: "conthrax",
-              color: "white",
-              textAlign: "center",
-              fontSize: 17,
-            }}
-          >
-            CRÉDITO
-          </Text>
-        </LinearGradient>
-      </TouchableOpacity>
-    </View>
+    </>
   );
 };
 
 // Estilos de la pantalla
 const styles = StyleSheet.create({
-  background: {
-    flex: 1,
+  tituloContainer: {
+    height: 105,
     backgroundColor: "white",
   },
   titulo: {
@@ -97,21 +111,24 @@ const styles = StyleSheet.create({
     marginLeft: 20,
     position: "absolute",
   },
-  scroll: {
+  scrollV: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+  scrollH: {
     height: 110,
     width: 353,
     left: 20,
     paddingTop: 6,
     position: "absolute",
-    top: 115,
   },
   cuadroInvertir: {
     backgroundColor: "#29364d",
     alignContent: "center",
     height: 80,
     width: 353,
+    top: 115,
     left: 20,
-    top: 230,
     borderRadius: 15,
   },
   cuadroCredito: {
@@ -119,11 +136,12 @@ const styles = StyleSheet.create({
     alignContent: "center",
     height: 80,
     width: 353,
+    top: 125,
     left: 20,
-    top: 240,
     borderRadius: 15,
     borderColor: "#D5D5D5",
     borderWidth: 1.5,
+    marginBottom: 140,
   },
   texto: {
     fontSize: 12,
