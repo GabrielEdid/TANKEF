53ddc030cbb428a4508ca08702aacfc3d9edefa3
GabrielEdid <gabrieledid28@gmail.com>
2024-01-10 15:36:46 -0600

    Ajustes de tamaño

--- src/screens/LogIn/OlivideContrasena.js
+++ src/screens/LogIn/OlivideContrasena.js
@@ -65,34 +65,29 @@ const OlvideContrasena = ({ navigation }) => {
             style={styles.back}
           />
         </TouchableOpacity>
-        <Image
-          source={require("../../../assets/images/Logo_Tankef.png")}
-          style={styles.imagen}
-        />
-        <Text style={styles.titulo}>TANKEF</Text>
-        {/* Contenedor Principal */}
-        <View style={styles.container}>
-          {/* Texto de Bienvenida a Olvide mi Contraseña */}
-          <Text
-            style={[
-              styles.texto,
-              { marginTop: 10, fontSize: 25, fontWeight: "bold" },
-            ]}
-          >
-            ¿Olvidaste tu contraseña?
-          </Text>
-          <Text style={styles.texto}>
-            ¡No te preocupes! Introduce tu correo y se te enviaran instrucciones
-            para modificar tu contraseña.
-          </Text>
-          {/* Input de Correo al que enviar el correo de restablecimiento */}
-          <TextInput
-            style={styles.input}
-            placeholder="Ingresa tu correo electrónico"
-            value={email}
-            onChangeText={setEmail}
-            autoCapitalize="none"
+        <View style={{ flex: 1 }}>
+          <Image
+            source={require("../../../assets/images/Logo_Tankef.png")}
+            style={styles.imagen}
           />
+          <Text style={styles.header}>TANKEF</Text>
+          {/* Contenedor Principal */}
+          <View style={styles.container}>
+            {/* Texto de Bienvenida a Olvide mi Contraseña */}
+            <Text style={styles.titulo}>¿Olvidaste tu contraseña?</Text>
+            <Text style={styles.texto}>
+              ¡No te preocupes! Introduce tu correo y se te enviaran
+              instrucciones para modificar tu contraseña.
+            </Text>
+            {/* Input de Correo al que enviar el correo de restablecimiento */}
+            <TextInput
+              style={styles.input}
+              placeholder="Ingresa tu correo electrónico"
+              value={email}
+              onChangeText={setEmail}
+              autoCapitalize="none"
+            />
+          </View>
         </View>
         {/* Boton de Enviar Correo */}
         <TouchableOpacity
@@ -122,24 +117,20 @@ const styles = StyleSheet.create({
     height: 90,
     alignSelf: "center",
     marginTop: 60,
-    position: "absolute",
   },
-  titulo: {
+  header: {
     fontFamily: "conthrax",
     fontSize: 25,
     color: "#29364d",
-    marginTop: 160,
+    marginTop: 10,
     alignSelf: "center",
-    position: "absolute",
   },
   container: {
-    position: "absolute",
+    marginTop: 25,
     alignSelf: "center",
-    height: 200,
-    width: 330,
-    marginTop: 210,
     backgroundColor: "white",
-    flex: 1,
+    width: "90%",
+    padding: 20,
     borderRadius: 15,
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 3 },
@@ -147,40 +138,45 @@ const styles = StyleSheet.create({
     shadowRadius: 5,
     elevation: 8,
   },
-  texto: {
+  titulo: {
     fontSize: 16,
     color: "#29364d",
     alignSelf: "center",
-    position: "absolute",
-    marginTop: 55,
     textAlign: "center",
-    paddingHorizontal: 12,
+    fontSize: 25,
+    fontWeight: "bold",
+  },
+  texto: {
+    top: 10,
+    fontSize: 16,
+    color: "#29364d",
+    alignSelf: "center",
+    textAlign: "center",
+    marginBottom: 25,
   },
   input: {
-    marginTop: 130,
     borderColor: "#29364d",
     borderWidth: 2,
     borderRadius: 10,
     alignSelf: "center",
-    width: 300,
+    width: "95%",
     height: 40,
     paddingLeft: 10,
     fontSize: 16,
   },
   botonGrande: {
-    marginTop: 750,
-    width: 350,
+    width: "85%",
     height: 60,
     alignSelf: "center",
     justifyContent: "center",
     backgroundColor: "#29364d",
     borderRadius: 25,
-    position: "absolute",
     shadowColor: "#000",
     shadowOffset: { width: 0, height: 6 },
     shadowOpacity: 0.37,
     shadowRadius: 5,
     elevation: 8,
+    marginBottom: 30,
   },
   textoBotonGrande: {
     color: "white",
