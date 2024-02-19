055a5d497de2dc87ddffe9b6f6f5f502da20a6dc
GabrielEdid <gabrieledid28@gmail.com>
2024-02-19 13:40:29 -0600

    Ajuste de ubicaciones

--- src/screens/Main/Perfil.js
+++ src/screens/Main/Perfil.js
@@ -17,7 +17,6 @@ import MaskedView from "@react-native-masked-view/masked-view";
 // Importaciones de Hooks y Componentes
 import { APIGet } from "../../API/APIService";
 import { UserContext } from "../../hooks/UserContext";
-import CuadroRedUsuario from "../../components/CuadroRedUsuario";
 import ProgressBar from "../../components/ProgressBar";
 import { Feather } from "@expo/vector-icons";
 import Post from "../../components/Post";
