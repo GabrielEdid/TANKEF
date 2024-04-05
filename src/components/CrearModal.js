7bbaa4979ea27ac0f1e31c1402ac48bf05c972e6
GabrielEdid <gabrieledid28@gmail.com>
2024-04-04 21:09:00 -0600

    Ajuste de navegacion

--- src/components/CrearModal.js
+++ src/components/CrearModal.js
@@ -75,7 +75,7 @@ const CrearModal = ({ isVisible, onClose }) => {
           onPress={() => {
             handleClose();
             navigation.navigate("Crear", {
-              screen: "Documentacion",
+              screen: "DefinirInversion",
               params: { flujo: "Inversión" },
             });
           }}
