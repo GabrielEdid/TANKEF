385aef5b91a2793e347cc1160e47296cf4245cf5
GabrielEdid <gabrieledid28@gmail.com>
2024-05-17 13:38:54 -0600

    Ajuste de navegacion

--- src/components/CrearModal.js
+++ src/components/CrearModal.js
@@ -89,7 +89,7 @@ const CrearModal = ({ isVisible, onClose }) => {
           onPress={() => {
             handleClose();
             navigation.navigate("Crear", {
-              screen: "ObligadosSolidarios",
+              screen: "DefinirCredito",
               params: { flujo: "Crédito" },
             });
           }}
